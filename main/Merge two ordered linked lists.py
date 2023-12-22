003a3f4c99c8f4ab5bd1bc1aae1a2956d829e35b
avhkbcj99 <ar75hv@hotmail.com>
2023-12-22 08:05:08 +0800

    Merge two ordered linked lists

---
+++ main/Merge two ordered linked lists.py
@@ -0,0 +1,46 @@
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+
+
+示例 1：
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Compare this snippet from main/merge%20two%20sorted%20lists.py:
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(-1, None)
+        cur = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next, l1 = l1, l1.next
+            else:
+                cur.next, l2 = l2, l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
