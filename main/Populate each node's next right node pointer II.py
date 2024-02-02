dd100ecc24216f388688d86615ba138d046e0ddc
avhkbcj99 <ar75hv@hotmail.com>
2024-02-02 09:13:04 +0800

    Populate each node's next right node pointer II

---
+++ main/Populate each node's next right node pointer II.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Date: 2024-02-02 09:11
+
+__author__ = "hello"
+
+"""
+给定一个二叉树：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL 。
+
+初始状态下，所有 next 指针都被设置为 NULL 。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,2,3,4,5,null,7]
+输出：[1,#,2,3,#,4,5,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next 指针连接），'#' 表示每层的末尾。
+示例 2：
+
+输入：root = []
+输出：[]
+ 
+
+提示：
+
+树中的节点数在范围 [0, 6000] 内
+-100 <= Node.val <= 100
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序的隐式栈空间不计入额外空间复杂度。
+"""
+from collections import deque
+
+
+class Node:
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        queue = deque([root])
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i < size - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
